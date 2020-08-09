8a07fdbff4ac739beeccb394bbfad4bbe653c8cf
Richard Kang <kangrc1@gmail.com>
2020-08-09 15:29:39 -0400

    Added pages

---
+++ Budgeter/Helpers/Controllers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Budgeter.Client.Helpers
+{
+    public static class Controllers
+    {
+        public static string ExpenseTable = "ExpenseTable";
+
+        public static string ExpenseDetails = "ExpenseDetails";
+    }
+}
